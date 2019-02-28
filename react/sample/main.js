5b904f1f436e2fea262ece64f34fc82626665cc3
0x4861 <0x4861@gmail.com>
2019-02-28 14:47:07 +0900

    [Mod] array

--- react/sample/main.js
+++ react/sample/main.js
@@ -1,52 +1,29 @@
-// component state 변화를 저장, 데이터를 활용
-// getInitialState -> component가 마운트 되기 전에 실행됨. 초기값을 정할때 사용.
-// componentWillMount -> 렌더링 되기전 실행됨.
-// componentDidMount -> 렌더링 후에 실행됨.
-// setState -> state값을 새로 셋팅할 때 사용.
-
-class AutoCounter extends React.Component {
-  constructor() {
-    super();
-    // timerCount 함수에서 this.setState 함수를 호출하기 위해서는, 초기화 과정에서 bind를 해줘야 한다.
-    this.timerCount=this.timerCount.bind(this);
-    this.state={count:10};
-  }
-
-  timerCount() {
-    this.setState({
-      count:this.state.count+10
-    });
-  }
-
-  componentDidMount(){
-    setInterval(this.timerCount, 1000);
-  }
-
+class Circle extends React.Component {
   render() {
+    var circleStyle={
+      padding:10,
+      margin:20,
+      display:"inline-block",
+      backgroundColor:this.props.bgColor,
+      width:100,
+      height:100,
+      borderRadius:"50%"
+    };
+
     return (
-      <h1>{this.state.count}</h1>
+      <div style={circleStyle}></div>
     );
   }
 };
 
-class AutoCounterDisplay extends React.Component {
-  render() {
-    var divStyle={
-      width:300,
-      textAlign:"center",
-      padding:40,
-      backgroundColor:"gray",
-      color:"blue",
-      borderRadius:20,
-      margin:20
-    }
-    return (
-      <div style={divStyle}>
-        <AutoCounter/>
-      </div>
-    );
-  }
-};
+function showCircle() {
+  var colors = ["orange", "blue", "green", "cyan"];
+  var randomColor = Math.floor(Math.random()*colors.length);
 
-const element = <AutoCounterDisplay/>;
-ReactDOM.render(element, document.querySelector('#container'));
+  return <Circle bgColor={colors[randomColor]}/>
+}
+
+var element = <Circle bgColor="red"/>;
+var element2 = <Circle bgColor="orange"/>;
+var destination = document.querySelector('#container');
+ReactDOM.render(<div>{showCircle()}{showCircle()}{showCircle()}</div>, destination);
