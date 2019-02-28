653330aec4918ca4701dae44b6266c30d43becd9
0x4861 <0x4861@gmail.com>
2019-02-28 14:58:32 +0900

    [Add] template class

--- react/sample/main.js
+++ react/sample/main.js
@@ -1,6 +1,6 @@
-class Circle extends React.Component {
+class TempClass extends React.Component {
   render() {
-    var circleStyle={
+    var tempStyle={
       padding:10,
       margin:20,
       display:"inline-block",
@@ -11,27 +11,12 @@ class Circle extends React.Component {
     };
 
     return (
-      <div style={circleStyle}></div>
+      <div style={tempStyle}></div>
     );
   }
 };
 
-// function showCircle() {
-//   var colors = ["orange", "blue", "green", "cyan"];
-//   var randomColor = Math.floor(Math.random()*colors.length);
-
-//   return <Circle bgColor={colors[randomColor]}/>
-// }
-var colors = ["orange", "blue", "green", "black", "lightgray", "red", "yellow"];
-
-var compData = [];
-for (var i=0; i<colors.length; i++) {
-  var color = colors[i];
-  compData.push(<Circle key={i+color} bgColor={color}/>);
-}
-
 var destination = document.querySelector('#container');
 ReactDOM.render(
   <div>
-    {compData}
   </div>, destination);
