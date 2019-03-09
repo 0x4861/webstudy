b7d3bb6db6492242fbcbcc8edaa0ea2f877be528
0x4861 <0x4861@gmail.com>
2019-03-10 04:43:04 +0900

    [Add] study about user event

--- react/src/components/search_bar.js
+++ react/src/components/search_bar.js
@@ -14,7 +14,11 @@ const SearchBar = () => {
 // class component
 class SearchBar extends Component {
   render() {
-    return <input />;
+    return <input onChange={this.handleInputChange}/>;
+  }
+
+  handleInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
