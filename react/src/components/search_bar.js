07f0724b0d96d1b5b2a3db8ff02b8d198bc118ea
0x4861 <0x4861@gmail.com>
2019-03-10 04:32:55 +0900

    [Mod] study about component

--- react/src/components/search_bar.js
+++ react/src/components/search_bar.js
@@ -1,9 +1,22 @@
-import React from 'react';
+// Syntactic sugar
+// import React, { Component } -> const Componet = React.Component
+import React, { Component } from 'react';
 
+// function component
+/*
 const SearchBar = () => {
   return (
     <input />
   )  
 };
+*/
+
+// class component
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+}
+
 
 export default SearchBar;
